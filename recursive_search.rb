f085ef92ec209287a494d117dd1c604c7091e567
jamot3 <34356804+jamot3@users.noreply.github.com>
2018-03-01 17:05:14 -0500

    Added file recursive_search
    Attempt #2 or #3 at recursion - not good. This is ugly code but I have to step away from it for a time as it's driving me crazy.

---
+++ recursive_search.rb
@@ -0,0 +1,38 @@
+### This is a holy mess, but I'm going to stop working on it for now because I stopped working on it. (Is that a recursion joke?) This method takes in an array and an element. If the array contains the element, it will return 'true', otherwise it will return 'false'. 
+
+def rec_search array, element
+    
+    while array.length > 1
+        new_array = []
+        x = array.length/2
+    
+        if array[x] > element
+            y = 0 
+            while y < x
+                new_array.push(array[y])
+                y+=1
+            end
+        else
+        
+            while x < array.length
+                new_array.push(array[x])
+                x+=1
+            end
+        end
+    
+    array = new_array
+    rec_search array, element
+    end
+
+
+    if array[0] == element
+        return true
+    end
+    
+    false
+end
+
+
+puts rec_search ["Aardvark", "Duck", "Deer", "Falcon"], "Falcon" ### == true
+puts rec_search ["Aardvark", "Duck", "Deer", "Falcon"], "Ocelot" ### == false
+puts rec_search ["Potato", "Tomato", "Carrot", "Grape", "Cherry"], "Carrot" ### == true
