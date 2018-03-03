4e9039cd205a6b0655343dd11877a39398c37e5c
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-03 15:14:12 -0500

    Massively improved method
    Well, after reading a bunch of blog posts and watch countless videos on recursion I stumbled across a post that reminded me that the 'slice' method existed. Now that I am not cutting the array in half through iteration, it's over 3x faster and more faithfully 'recursive.'  Let this be a reminder to future me:  learning doesn't happens in a vacuum, use all available resources!

--- recursive_search.rb
+++ recursive_search.rb
@@ -1,38 +1,33 @@
-### This is a holy mess, but I'm going to stop working on it for now because I stopped working on it. (Is that a recursion joke?) This method takes in an array and an element. If the array contains the element, it will return 'true', otherwise it will return 'false'.
+### This method takes in an array and an element. If the array contains the element, it will return 'true', 
+### otherwise it will return 'false'.
 
 def rec_search array, element
     
     if array.length <= 1
         return array[0] == element
     end
-    
-    new_array = []
+        
+        
+        
     x = array.length/2
     
     if array[x] > element
-        y = 0 
-        while y < x
-            new_array.push(array[y])
-            y+=1
-        end
+        rec_search array.slice(0, x), element
     else
-        while x < array.length
-            new_array.push(array[x])
-            x+=1
-        end
+        rec_search array.slice(x, array.length - 1), element
     end
     
-    array = new_array
-    rec_search array, element
 
 end
 
 
-### As it stands, it feels like this code is completely terrible. It takes FOREVER to search for an element. 
-### I ran it 10,000 times on an array consisting of 223 elements, and it took .251794 seconds to compute. 
-### Compare this to the simple, non-recursive 'while' loop code below, which completes 10,000 iterations of
-### the same code in .015628 seconds. I do not believe I can beat the 'while' loop w recursion in this instance,
-### but it's good practice. 
+### Okay, the above code is as 'optimized' a recursive search function as I am capable of, at the moment.
+### Searching an array with a length of 224 elements 10,000 times, the above method took .093 seconds.
+### The below method performs the same 10,000 repetitions in just .016 seconds. However, before I realized I could
+### utilize the 'slice' method  for this function (thanks to scouring ennumerable blog posts related to Ruby & recursion)
+### the above recursive method clocked in at a whopping .28 seconds. One step at a time. 
+
+
 
 def search array, element
     x = 0 
@@ -46,3 +41,35 @@ def search array, element
     
     false
 end
+
+
+
+### BELOW - first attempt at a recursive search function. Very slow, very inefficient - forgot about Ruby's wonderful 'slice' method. 
+
+#def rec_search array, element
+    
+#    if array.length <= 1
+#       return array[0] == element
+#    end
+ 
+#    new_array = []
+#    x = array.length/2
+    
+ 
+#    if array[x] > element
+#        y = 0 
+#        while y < x
+#            new_array.push(array[y])
+#            y+=1
+#        end
+#    else
+#       while x < array.length
+#           new_array.push(array[x])
+#           x+=1
+#       end
+#   end
+#   
+#   array = new_array
+#   rec_search array, element
+#
+#end
