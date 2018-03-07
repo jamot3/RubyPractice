365a2598ac30c76e99ae85ae9e1ae7b3492f0eb0
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-06 20:12:43 -0500

    Added file
    This is an exercise from Chris Pine's 'Learn to Program.' I am a bit fuzzy from post-op meds, just had major sinus surgery, but I wanted to keep my daily contribution streak alive so I figured I'd do some algorithm practice.

---
+++ recursive_scramble.rb
@@ -0,0 +1,23 @@
+def scramble array
+    scram string, []
+end
+
+
+def scram unscrambled, scrambled
+    ran_index = rand(unscrambled.length - 1)
+    
+    
+end
+
+unscrambled = [1, 3, 4, 5, 6]
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
+puts rand(unscrambled.length)
