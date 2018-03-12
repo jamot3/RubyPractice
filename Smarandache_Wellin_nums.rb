993253bb251e4ab6b7c016034e5be5c040911184
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-11 22:33:52 -0400

    Corrected description

--- Smarandache_Wellin_nums.rb
+++ Smarandache_Wellin_nums.rb
@@ -1,11 +1,11 @@
 ### Smarandache–Wellin numbers are numbers that consist of the primes of that base. e.g. four digit
-### SW number in base 10 is 2357. Five digit is 235711. The below function will return SW numbers
-### up to a given number of digits in length. 
+### SW number in base 10 is 2357. The next digit is 235711. The below function will return a given 
+### number of SW numbers.  
 
-def sw digits
+def sw howmany
     primes = []
     number = 0 
-    while primes.length < digits
+    while primes.length < howmany
         if ((number % 2 == 0 && number != 2) || (number % 3 == 0 && number != 3) || (number <= 1)) == false
             primes.push(number)
         end
@@ -15,7 +15,7 @@ def sw digits
     sw = []
     number = ""
     x = 0 
-    while number.length < digits
+    while number.length < howmany
         number += primes[x].to_s
         sw.push(number)
         x+=1
