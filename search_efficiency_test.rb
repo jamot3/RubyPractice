5f50218db7bf78243990cac18163c28a06b0c317
Joe M <34356804+jamot3@users.noreply.github.com>
2018-03-14 16:45:59 -0400

    Uploaded file.
    This code contains a binary search (rec_search) and a simple indexing search (search). Below that is benchmark testing code that returns the time (averaged over 100 instances) that it takes each function to find an element 10,000 times. Both algorithms are tested 
    along with Ruby's .include? method.

---
+++ search_efficiency_test.rb
@@ -0,0 +1,473 @@
+### This code contains a binary search (rec_search) and a simple indexing search (search). 
+### Below that is benchmark testing code that returns the time (averaged over 100 instances)
+### that it takes each function to find an element 10,000 times. Both algorithms are tested 
+### along with Ruby's .include? method. 
+
+array_short =
+    ["Aardvark",
+    "Albatross",
+    "Alligator",
+    "Alpaca",
+    "Ant",
+    "Anteater",
+    "Antelope",
+    "Ape",
+    "Armadillo",
+    "Donkey",
+    "Baboon",
+    "Badger",
+    "Barracuda",
+    "Bat",
+    "Bear",
+    "Beaver",
+    "Bee",
+    "Bison",
+    "Boar",
+    "Buffalo",
+    "Butterfly",
+    "Camel",
+    "Capybara",
+    "Caribou",
+    "Cassowary",
+    "Cat",
+    "Caterpillar",
+    "Cattle",
+    "Chamois",
+    "Cheetah",
+    "Chicken",
+    "Chimpanzee",
+    "Chinchilla",
+    "Chough",
+    "Clam",
+    "Cobra",
+    "Cockroach",
+    "Cod",
+    "Cormorant",
+    "Coyote",
+    "Crab",
+    "Crane",
+    "Crocodile",
+    "Crow",
+    "Curlew",
+    "Deer",
+    "Dinosaur",
+    "Dog",
+    "Dogfish",
+    "Dolphin",
+    "Dotterel",
+    "Dove",
+    "Dragonfly",
+    "Duck",
+    "Dugong",
+    "Dunlin",
+    "Eagle",
+    "Echidna",
+    "Eel",
+    "Eland",
+    "Elephant",
+    "Elk",
+    "Emu",
+    "Falcon",
+    "Ferret",
+    "Finch",
+    "Fish",
+    "Flamingo",
+    "Fly",
+    "Fox",
+    "Frog",
+    "Gaur",
+    "Gazelle",
+    "Gerbil",
+    "Giraffe",
+    "Gnat",
+    "Gnu",
+    "Goat",
+    "Goldfinch",
+    "Goldfish",
+    "Goose",
+    "Gorilla",
+    "Goshawk",
+    "Grasshopper",
+    "Grouse",
+    "Guanaco",
+    "Gull",
+    "Hamster",
+    "Hare",
+    "Hawk",
+    "Hedgehog",
+    "Heron",
+    "Herring",
+    "Hippopotamus",
+    "Hornet",
+    "Horse",
+    "Human",
+    "Hummingbird",
+    "Tapir",
+    "Tarsier",
+    "Termite",
+    "Tiger",
+    "Toad",
+    "Trout",
+    "Turkey",
+    "Turtle",
+    "Viper",
+    "Vulture",
+    "Wallaby",
+    "Walrus",
+    "Wasp",
+    "Weasel",
+    "Whale",
+    "Wildcat",
+    "Wolf",
+    "Wolverine",
+    "Wombat",
+    "Woodcock",
+    "Woodpecker",
+    "Worm",
+    "Wren",
+    "Yak",
+    "Zebra"]
+array_long = 
+    ["Aardvark",
+    "Albatross",
+    "Alligator",
+    "Alpaca",
+    "Ant",
+    "Anteater",
+    "Antelope",
+    "Ape",
+    "Armadillo",
+    "Donkey",
+    "Baboon",
+    "Badger",
+    "Barracuda",
+    "Bat",
+    "Bear",
+    "Beaver",
+    "Bee",
+    "Bison",
+    "Boar",
+    "Buffalo",
+    "Butterfly",
+    "Camel",
+    "Capybara",
+    "Caribou",
+    "Cassowary",
+    "Cat",
+    "Caterpillar",
+    "Cattle",
+    "Chamois",
+    "Cheetah",
+    "Chicken",
+    "Chimpanzee",
+    "Chinchilla",
+    "Chough",
+    "Clam",
+    "Cobra",
+    "Cockroach",
+    "Cod",
+    "Cormorant",
+    "Coyote",
+    "Crab",
+    "Crane",
+    "Crocodile",
+    "Crow",
+    "Curlew",
+    "Deer",
+    "Dinosaur",
+    "Dog",
+    "Dogfish",
+    "Dolphin",
+    "Dotterel",
+    "Dove",
+    "Dragonfly",
+    "Duck",
+    "Dugong",
+    "Dunlin",
+    "Eagle",
+    "Echidna",
+    "Eel",
+    "Eland",
+    "Elephant",
+    "Elk",
+    "Emu",
+    "Falcon",
+    "Ferret",
+    "Finch",
+    "Fish",
+    "Flamingo",
+    "Fly",
+    "Fox",
+    "Frog",
+    "Gaur",
+    "Gazelle",
+    "Gerbil",
+    "Giraffe",
+    "Gnat",
+    "Gnu",
+    "Goat",
+    "Goldfinch",
+    "Goldfish",
+    "Goose",
+    "Gorilla",
+    "Goshawk",
+    "Grasshopper",
+    "Grouse",
+    "Guanaco",
+    "Gull",
+    "Hamster",
+    "Hare",
+    "Hawk",
+    "Hedgehog",
+    "Heron",
+    "Herring",
+    "Hippopotamus",
+    "Hornet",
+    "Horse",
+    "Human",
+    "Hummingbird",
+    "Hyena",
+    "Ibex",
+    "Ibis",
+    "Jackal",
+    "Jaguar",
+    "Jay",
+    "Jellyfish",
+    "Kangaroo",
+    "Kingfisher",
+    "Koala",
+    "Kookabura",
+    "Kouprey",
+    "Kudu",
+    "Lapwing",
+    "Lark",
+    "Lemur",
+    "Leopard",
+    "Lion",
+    "Llama",
+    "Lobster",
+    "Locust",
+    "Loris",
+    "Louse",
+    "Lyrebird",
+    "Magpie",
+    "Mallard",
+    "Manatee",
+    "Mandrill",
+    "Mantis",
+    "Marten",
+    "Meerkat",
+    "Mink",
+    "Mole",
+    "Mongoose",
+    "Monkey",
+    "Moose",
+    "Mosquito",
+    "Mouse",
+    "Mule",
+    "Narwhal",
+    "Newt",
+    "Nightingale",
+    "Octopus",
+    "Okapi",
+    "Opossum",
+    "Oryx",
+    "Ostrich",
+    "Otter",
+    "Owl",
+    "Oyster",
+    "Panther",
+    "Parrot",
+    "Partridge",
+    "Peafowl",
+    "Pelican",
+    "Penguin",
+    "Pheasant",
+    "Pig",
+    "Pigeon",
+    "Pony",
+    "Porcupine",
+    "Porpoise",
+    "Quail",
+    "Quelea",
+    "Quetzal",
+    "Rabbit",
+    "Raccoon",
+    "Rail",
+    "Ram",
+    "Rat",
+    "Raven",
+    "Red deer",
+    "Red panda",
+    "Reindeer",
+    "Rhinoceros",
+    "Rook",
+    "Salamander",
+    "Salmon",
+    "Sand Dollar",
+    "Sandpiper",
+    "Sardine",
+    "Scorpion",
+    "Seahorse",
+    "Seal",
+    "Shark",
+    "Sheep",
+    "Shrew",
+    "Skunk",
+    "Snail",
+    "Snake",
+    "Sparrow",
+    "Spider",
+    "Spoonbill",
+    "Squid",
+    "Squirrel",
+    "Starling",
+    "Stingray",
+    "Stinkbug",
+    "Stork",
+    "Swallow",
+    "Swan",
+    "Tapir",
+    "Tarsier",
+    "Termite",
+    "Tiger",
+    "Toad",
+    "Trout",
+    "Turkey",
+    "Turtle",
+    "Viper",
+    "Vulture",
+    "Wallaby",
+    "Walrus",
+    "Wasp",
+    "Weasel",
+    "Whale",
+    "Wildcat",
+    "Wolf",
+    "Wolverine",
+    "Wombat",
+    "Woodcock",
+    "Woodpecker",
+    "Worm",
+    "Wren",
+    "Yak",
+    "Zebra"]
+    
+
+def rec_search array, element
+    
+    if array.length <= 1
+        return array[0] == element
+    end
+          
+    x = array.length/2
+    
+    if array[x] > element
+        rec_search array.slice(0, x), element
+    else
+        rec_search array.slice(x, array.length - 1), element
+    end
+ 
+end
+   
+def search array, element
+    x = 0 
+    
+    while x < array.length
+        if array[x] == element
+            return true
+        end
+        x+=1
+    end
+    false
+end
+ 
+time_array = []
+ 100.times do 
+require "benchmark"
+time = Benchmark.realtime do
+10000.times do array_short.include?("Viper")
+end
+end
+time_array.push(time)
+end
+sum = 0 
+time_array.each {|element| (sum+=element)}
+average_include = sum/time_array.length
+puts "include? (112):  #{average_include}"
+
+
+time_array = []
+ 100.times do 
+require "benchmark"
+time = Benchmark.realtime do
+10000.times do rec_search array_short, "Viper"
+end
+end
+time_array.push(time)
+end
+sum = 0 
+time_array.each {|element| (sum+=element)}
+average_rec = sum/time_array.length
+puts "recursive (112):  #{average_rec}"
+
+
+time_array = []
+ 100.times do 
+require "benchmark"
+time = Benchmark.realtime do
+10000.times do search array_short, "Viper"
+end
+end
+time_array.push(time)
+end
+sum = 0 
+time_array.each {|element| (sum+=element)}
+average_loop = sum/time_array.length
+puts "index (112):  #{average_loop}"
+
+
+
+time_array = []
+ 100.times do 
+require "benchmark"
+time = Benchmark.realtime do
+10000.times do array_long.include?("Viper")
+end
+end
+time_array.push(time)
+end
+sum = 0 
+time_array.each {|element| (sum+=element)}
+average_include = sum/time_array.length
+puts "include? (224):  #{average_include}"
+
+
+time_array = []
+ 100.times do 
+require "benchmark"
+time = Benchmark.realtime do
+10000.times do rec_search array_long, "Viper"
+end
+end
+time_array.push(time)
+end
+sum = 0 
+time_array.each {|element| (sum+=element)}
+average_rec = sum/time_array.length
+puts "recursive (224):  #{average_rec}"
+
+
+time_array = []
+ 100.times do 
+require "benchmark"
+time = Benchmark.realtime do
+10000.times do search array_long, "Viper"
+end
+end
+time_array.push(time)
+end
+sum = 0 
+time_array.each {|element| (sum+=element)}
+average_loop = sum/time_array.length
+puts "index (224):  #{average_loop}"
